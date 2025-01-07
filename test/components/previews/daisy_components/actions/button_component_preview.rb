a79bc10ce1db408ab94b08b14e7f405aa6c8ce6e
Nittarab <p.barattin@gmail.com>
2025-01-07 19:21:11 +0100

    Improve button preview

--- test/components/previews/daisy_components/actions/button_component_preview.rb
+++ test/components/previews/daisy_components/actions/button_component_preview.rb
@@ -5,66 +5,117 @@
 module DaisyComponents
   module Actions
     class ButtonComponentPreview < Lookbook::Preview
-      # Primary button
-      # ---------------
-      # This is the default button style you should use for most actions.
+      # @!group Playground
+
+      # @title Default
+      # @description Basic button example with all available options
       #
-      # @label Primary Button
-      def default
-        render DaisyComponents::Actions::ButtonComponent.new do
-          'Click me'
-        end
+      # @param content text
+      # @param variant [String] select [neutral, primary, secondary, accent, info, success, warning, error, ghost, link]
+      # @param size [String] select [xs, sm, md, lg]
+      # @param disabled toggle
+      # @param classes text
+      def playground(
+        content: 'Click me',
+        variant: 'primary',
+        size: 'md',
+        disabled: false,
+        classes: ''
+      )
+        render(DaisyComponents::Actions::ButtonComponent.new(
+                 variant: variant,
+                 size: size,
+                 disabled: disabled,
+                 classes: classes
+               )) { content }
       end
 
-      # Button variants
+      # @!endgroup
+
+      # @!group Variants
+
+      # Button Variants
       # ---------------
-      # DaisyUI provides multiple button variants for different contexts
+      # All available button variants
       #
-      # @label Variants
+      # @label All Variants
       def variants
         render_with_template(locals: {
                                variants: DaisyComponents::Actions::ButtonComponent::VARIANTS
                              })
       end
 
-      # Button sizes
+      # Primary Button
       # ---------------
-      # Buttons come in different sizes to fit your needs
+      # Use for main actions
+      def primary
+        render DaisyComponents::Actions::ButtonComponent.new(variant: :primary) do
+          'Primary Action'
+        end
+      end
+
+      # Secondary Button
+      # ---------------
+      # Use for secondary actions
+      def secondary
+        render DaisyComponents::Actions::ButtonComponent.new(variant: :secondary) do
+          'Secondary Action'
+        end
+      end
+
+      # Ghost Button
+      # ---------------
+      # Use for subtle actions
+      def ghost
+        render DaisyComponents::Actions::ButtonComponent.new(variant: :ghost) do
+          'Ghost Button'
+        end
+      end
+
+      # @!endgroup
+
+      # @!group Sizes
+
+      # Button Sizes
+      # ---------------
+      # All available button sizes
       #
-      # @label Sizes
+      # @label All Sizes
       def sizes
         render_with_template(locals: {
                                sizes: DaisyComponents::Actions::ButtonComponent::SIZES
                              })
       end
 
-      # Interactive button
-      # ---------------
-      # Try different button combinations
-      #
-      # @label Playground
-      # @param variant select [neutral, primary, secondary, accent, info, success, warning, error, ghost, link]
-      # @param size select [xs, sm, md, lg]
-      # @param text text "Button text"
-      # @param disabled toggle
-      def playground(variant: :primary, size: :md, text: 'Click me', disabled: false)
-        render DaisyComponents::Actions::ButtonComponent.new(variant: variant, size: size, disabled: disabled) do
-          text
-        end
-      end
+      # @!endgroup
 
-      # @!group Examples
+      # @!group States
 
-      # Loading state
+      # Loading State
       # ---------------
+      # Button with loading animation
       def loading
         render DaisyComponents::Actions::ButtonComponent.new(classes: 'loading') do
           'Loading...'
         end
       end
 
-      # With icon
+      # Disabled State
       # ---------------
+      # Button in disabled state
+      def disabled
+        render DaisyComponents::Actions::ButtonComponent.new(disabled: true) do
+          "Can't click me"
+        end
+      end
+
+      # @!endgroup
+
+      # @!group Common Use Cases
+
+      # With Icon
+      # ---------------
+      # Button with an icon
       def with_icon
         render DaisyComponents::Actions::ButtonComponent.new do
           safe_join([
@@ -74,11 +125,25 @@ module DaisyComponents
         end
       end
 
-      # Block button
+      # Block Button
       # ---------------
+      # Full-width button
       def block
         render DaisyComponents::Actions::ButtonComponent.new(classes: 'btn-block') do
           'Full width button'
+        end
+      end
+
+      # Icon Only
+      # ---------------
+      # Button with only an icon
+      def icon_only
+        render DaisyComponents::Actions::ButtonComponent.new(classes: 'btn-square') do
+          <<~SVG.html_safe
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+          SVG
         end
       end
 
