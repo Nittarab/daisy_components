8b9a51ba987ffaf8ec6070ed3639a3370aac0c78
Nittarab <p.barattin@gmail.com>
2025-01-21 22:06:29 +0100

    Use prview for tests for card component

--- test/components/daisy_components/component_test_case.rb
+++ test/components/daisy_components/component_test_case.rb
@@ -11,5 +11,23 @@ module DaisyComponents
       super
       @routes = DaisyComponents::Engine.routes
     end
+
+    private
+
+    def preview_class
+      "#{self.class.name.sub(/Test$/, '')}Preview".constantize
+    end
+
+    def preview
+      @preview ||= preview_class.new
+    end
+
+    def render_preview(preview_class, example, **params)
+      preview = preview_class.new
+      preview_result = preview.public_send(example, **params)
+      component = preview_result[:component]
+      preview_result[:block].call(component) if preview_result[:block]
+      render_inline(component)
+    end
   end
 end
