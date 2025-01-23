959d9ef57106df2075a99176804a988255c61ad5
Nittarab <p.barattin@gmail.com>
2025-01-23 20:58:47 +0100

    Improve Accortion and Avatar component tests and view

--- test/components/daisy_components/data_display/accordion_component_test.rb
+++ test/components/daisy_components/data_display/accordion_component_test.rb
@@ -6,121 +6,94 @@ module DaisyComponents
   module DataDisplay
     class AccordionComponentTest < DaisyComponents::ComponentTestCase
       def test_renders_basic_accordion
-        render_inline(AccordionComponent.new) do |component|
-          component.with_item(title: 'Item 1') { 'Content 1' }
-          component.with_item(title: 'Item 2') { 'Content 2' }
-        end
+        render_preview(:basic)
+        assert_selector('.collapse')
+        assert_selector('.collapse-title', text: 'Click me')
+        assert_selector('.collapse-content', text: 'Hidden content')
+        assert_selector('input[type="checkbox"]')
+      end
 
-        assert_selector('.collapse', count: 2)
+      def test_renders_multiple_items
+        render_preview(:multiple)
+        assert_selector('.collapse', count: 3)
         assert_selector('.collapse-title', text: 'Item 1')
         assert_selector('.collapse-title', text: 'Item 2')
+        assert_selector('.collapse-title', text: 'Item 3')
         assert_selector('.collapse-content', text: 'Content 1')
         assert_selector('.collapse-content', text: 'Content 2')
-        assert_selector('input[type="checkbox"]', count: 2)
+        assert_selector('.collapse-content', text: 'Content 3')
+        assert_selector('input[checked]')
       end
 
       def test_renders_joined_accordion
-        render_inline(AccordionComponent.new(join: true)) do |component|
-          component.with_item(title: 'Item 1') { 'Content 1' }
-          component.with_item(title: 'Item 2') { 'Content 2' }
-        end
-
+        render_preview(:joined)
         assert_selector('.join')
-        assert_selector('.join-item', count: 2)
+        assert_selector('.join-item', count: 3)
         assert_selector('.join.join-vertical')
-        assert_selector('.collapse.join-item', count: 2)
+        assert_selector('.collapse.join-item', count: 3)
       end
 
       def test_renders_with_arrow
-        render_inline(AccordionComponent.new(arrow: true)) do |component|
-          component.with_item(title: 'Item 1') { 'Content 1' }
-        end
-
+        render_preview(:with_arrow)
         assert_selector('.collapse.collapse-arrow')
+        assert_selector('.collapse-title', text: 'Expand me')
+        assert_selector('.collapse-title', text: 'Expand me too')
         refute_selector('.collapse-plus')
       end
 
       def test_renders_with_plus
-        render_inline(AccordionComponent.new(plus: true)) do |component|
-          component.with_item(title: 'Item 1') { 'Content 1' }
-        end
-
+        render_preview(:with_plus)
         assert_selector('.collapse.collapse-plus')
+        assert_selector('.collapse-title', text: 'Click to expand')
+        assert_selector('.collapse-title', text: 'Click me as well')
         refute_selector('.collapse-arrow')
       end
 
       def test_renders_with_radio
-        render_inline(AccordionComponent.new(radio: true)) do |component|
-          component.with_item(title: 'Item 1', name: 'group1') { 'Content 1' }
-          component.with_item(title: 'Item 2', name: 'group1') { 'Content 2' }
-        end
-
-        assert_selector('input[type="radio"][name="group1"]', count: 2)
-        refute_selector('input[type="checkbox"]')
-      end
-
-      def test_renders_with_checked_item
-        render_inline(AccordionComponent.new) do |component|
-          component.with_item(title: 'Item 1', checked: true) { 'Content 1' }
-        end
-
+        render_preview(:radio_group)
+        assert_selector('input[type="radio"][name="radio-group"]', count: 3)
+        assert_selector('.collapse-title', text: 'Option 1')
+        assert_selector('.collapse-title', text: 'Option 2')
+        assert_selector('.collapse-title', text: 'Option 3')
         assert_selector('input[checked]')
+        refute_selector('input[type="checkbox"]')
       end
 
       def test_renders_with_custom_classes
         render_inline(AccordionComponent.new(class: 'custom-class')) do |component|
           component.with_item(title: 'Item 1') { 'Content 1' }
         end
-
         assert_selector('.custom-class')
       end
 
-      def test_renders_with_complex_content
-        render_complex_content_test
-        assert_complex_content_selectors
+      def test_renders_with_list
+        render_preview(:with_list)
+        assert_selector('.collapse-content ul.list-disc.list-inside')
+        assert_selector('.collapse-content li', count: 3)
+        assert_selector('.collapse-content li', text: 'First item')
+        assert_selector('.collapse-content li', text: 'Second item')
+        assert_selector('.collapse-content li', text: 'Third item')
       end
 
-      def test_renders_with_nested_components
-        render_nested_components_test
-        assert_nested_components_selectors
-      end
-
-      private
-
-      def render_complex_content_test
-        render_inline(AccordionComponent.new) do |component|
-          component.with_item(title: 'With List') do
-            tag.ul(class: 'list-disc') do
-              safe_join([
-                          tag.li('First item'),
-                          tag.li('Second item')
-                        ])
-            end
-          end
-        end
-      end
-
-      def assert_complex_content_selectors
-        assert_selector('.collapse-content ul.list-disc')
-        assert_selector('.collapse-content li', count: 2)
-      end
-
-      def render_nested_components_test
-        render_inline(AccordionComponent.new) do |component|
-          component.with_item(title: 'With Card') do
-            tag.div(class: 'card') do
-              tag.div(class: 'card-body') do
-                tag.h2('Card Title', class: 'card-title')
-              end
-            end
-          end
-        end
-      end
-
-      def assert_nested_components_selectors
+      def test_renders_with_card
+        render_preview(:with_card)
         assert_selector('.collapse-content .card')
         assert_selector('.collapse-content .card-body')
-        assert_selector('.collapse-content .card-title', text: 'Card Title')
+        assert_selector('.collapse-content .card-title', text: 'Simple Card')
+      end
+
+      def test_playground_renders_with_all_options
+        render_preview(:playground, params: {
+                         join: true,
+                         arrow: true,
+                         plus: false,
+                         radio: false,
+                         classes: 'custom-class'
+                       })
+
+        assert_selector('.join')
+        assert_selector('.collapse.collapse-arrow')
+        assert_selector('.custom-class')
       end
     end
   end
