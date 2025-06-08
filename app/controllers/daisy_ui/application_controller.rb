8457002012379933915b580e46e4c0fe6170f807
Nittarab <p.barattin@gmail.com>
2025-06-08 18:00:53 +0200

    Remove useless file

--- app/controllers/daisy_ui/application_controller.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module DaisyUI
-  class ApplicationController < ActionController::Base
-  end
-end
--- app/helpers/daisy_ui/application_helper.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module DaisyUI
-  module ApplicationHelper
-  end
-end
